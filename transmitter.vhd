e7f864d26d6b93fbfb9bed9652d249a32ea3478a
Greg Medding <greg@medding.net>
2023-01-11 12:42:56 -0800

    Resolving many synthesis and simulation issues
    Making sure registers are only set on one clock edge, fixing issues
    found in the simulation (e.g. tx signal changing at wrong time).
    
    Known issue: still won't synthesize because of one bit in the
    current_tx_data register (bit 0).

--- transmitter.vhd
+++ transmitter.vhd
@@ -59,27 +59,37 @@ architecture Behavioral of transmitter is
 	
 	signal state : txstate := tx_idle;
 	
-	signal start_bits_remaining : std_logic := '0';
-	signal tx_bits_remaining : std_logic_vector (3 downto 0) := "0000";
-	signal current_tx_data : std_logic_vector (7 downto 0) := "00000000";
-	signal parity_bits_remaining : std_logic := '0';
+	-- Counting registers tracking bits sent in each phase
+	signal start_bits_sent : std_logic := '0';
+	signal tx_bits_sent : std_logic_vector (3 downto 0) := "0000";
+	signal parity_bits_sent : std_logic := '0';
+	signal stop_bits_sent : std_logic_vector (1 downto 0) := "00";
+	
+	-- Registers to capture current TX settings on transition from idle to transmit
+	signal expected_start_bits : std_logic := '0';
+	signal expected_tx_bits : std_logic_vector (3 downto 0) := "0000";
+	signal expected_parity_bits : std_logic := '0';
+	signal expected_stop_bits : std_logic_vector (1 downto 0) := "00";
+	signal expected_tx_data : std_logic_vector (7 downto 0) := "00000000";
+	
+	
+	signal current_tx_data : std_logic_vector (6 downto 0) := "0000000";
 	signal current_tx_parity : std_logic := '0';
-	signal stop_bits_remaining : std_logic_vector (1 downto 0) := "00";
+	signal next_tx_bit : std_logic := '0';
 
 begin
 	-- Bit clock generator
 	process (clk, bit_clk_en) begin
-		if rising_edge(clk) and bit_clk_en = '1' then
-			if clk_counter = current_clk_div then
+		if rising_edge(clk) then
+			if bit_clk_en = '1' and clk_counter = current_clk_div then
 				clk_counter <= "0000000000000000000";
 				bit_clk <= not bit_clk;
-			else
+			elsif bit_clk_en = '1' then
 				clk_counter <= clk_counter + 1;
+			else
+				bit_clk <= '0';
+				clk_counter <= "0000000000000000000";
 			end if;
-			
-		elsif bit_clk_en = '0' then
-			bit_clk <= '0';
-			clk_counter <= "0000000000000000000";
 		end if;
 	end process;
 	
@@ -91,6 +101,8 @@ begin
 				when tx_idle =>
 					if txstart = '1' then
 						current_clk_div <= clk_div;
+						
+						expected_tx_data <= data;
 
 						if parity = "01" then
 							current_tx_parity <= data(7) xor data(6) xor data(5) xor data(4) xor data(3) xor data(2) xor data(1) xor data(0);
@@ -100,18 +112,38 @@ begin
 							current_tx_parity <= '0';
 						end if;
 						
+						expected_start_bits <= '1';
+						
+						if eight_data = '1' then
+							expected_tx_bits <= "1000";
+						else
+							expected_tx_bits <= "0111";
+						end if;
+
+						if parity = "01" or parity = "10" then
+							expected_parity_bits <= '1';
+						else
+							expected_parity_bits <= '0';
+						end if;
+						
+						if two_stop = '1' then
+							expected_stop_bits <= "10";
+						else
+							expected_stop_bits <= "01";
+						end if;
+						
 						bit_clk_en <= '1';
 						state <= tx_start;
 					end if;
 					
 				when tx_start =>
-					if start_bits_remaining = '0' then
+					if start_bits_sent = expected_start_bits then
 						state <= tx_transmit;
 					end if;
 					
 				when tx_transmit =>
-					if tx_bits_remaining = "0000" then
-						if parity_bits_remaining = '1' then
+					if tx_bits_sent = expected_tx_bits then
+						if expected_parity_bits = '1' then
 							state <= tx_parity;
 						else
 							state <= tx_stop;
@@ -119,12 +151,12 @@ begin
 					end if;
 					
 				when tx_parity =>
-					if parity_bits_remaining = '0' then
+					if parity_bits_sent = expected_parity_bits then
 						state <= tx_stop;
 					end if;
 					
 				when tx_stop =>
-					if stop_bits_remaining = "00" then
+					if stop_bits_sent = expected_stop_bits then
 						bit_clk_en <= '0';
 						state <= tx_idle;
 					end if;
@@ -138,44 +170,27 @@ begin
 		if falling_edge(bit_clk) then
 			case state is
 				when tx_start =>
-					start_bits_remaining <= '0';
+					start_bits_sent <= '1';
+					-- Prepare for the last stage of transmit
+					stop_bits_sent <= "00";
 				when tx_transmit =>
-					tx_bits_remaining <= tx_bits_remaining - 1;
+					tx_bits_sent <= tx_bits_sent + 1;
 				when tx_parity =>
-					parity_bits_remaining <= '0';
+					parity_bits_sent <= '1';
 				when tx_stop =>
-					stop_bits_remaining <= stop_bits_remaining - 1;
+					stop_bits_sent <= stop_bits_sent + 1;
+					-- Cleanup while we still have a clock
+					start_bits_sent <= '0';
+					tx_bits_sent <= "0000";
+					parity_bits_sent <= '0';
 				when others =>
-					-- do nothing
+					-- Nothing to do here
 			end case;
-		elsif rising_edge(bit_clk_en) then
-			start_bits_remaining <= '1';
-			
-			if eight_data = '1' then
-				tx_bits_remaining <= "1000";
-			else
-				tx_bits_remaining <= "0111";
-			end if;
-
-			if parity = "01" or parity = "10" then
-				parity_bits_remaining <= '1';
-			else
-				parity_bits_remaining <= '0';
-			end if;
-			
-			if two_stop = '1' then
-				stop_bits_remaining <= "10";
-			else
-				stop_bits_remaining <= "01";
-			end if;
 		end if;
 	end process;
 	
 	-- TX data generator
-	process (bit_clk, bit_clk_en, state) begin		
-		if rising_edge(bit_clk_en) then
-			current_tx_data <= data;
-		end if;
+	process (bit_clk, bit_clk_en, state) begin
 			
 		case state is
 		
@@ -187,11 +202,23 @@ begin
 				txdata <= '1';
 				txready <= '0';
 				
+				if falling_edge(bit_clk) then
+					current_tx_data(6 downto 0) <= expected_tx_data(7 downto 1);
+				end if;
+				
+				if rising_edge(bit_clk) then
+					next_tx_bit <= expected_tx_data(0);
+				end if;
+				
 			when tx_transmit =>
-				txdata <= current_tx_data(0);
+				txdata <= next_tx_bit;
 				txready <= '0';
 				if falling_edge(bit_clk) then
-					current_tx_data(6 downto 0) <= current_tx_data(7 downto 1);
+					current_tx_data(5 downto 0) <= current_tx_data(6 downto 1);
+				end if;
+				
+				if rising_edge(bit_clk) then
+					next_tx_bit <= current_tx_data(0);
 				end if;
 				
 			when tx_parity =>
